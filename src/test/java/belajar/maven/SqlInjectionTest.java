653765a1d3371809abd8f5e106ce519078d5cfcc
Rhendrik Rian <rhendrik20@gmail.com>
2024-01-09 22:24:12 +0700

    Membuat SqlInjectionTest.java

---
+++ src/test/java/belajar/maven/SqlInjectionTest.java
@@ -0,0 +1,39 @@
+package belajar.maven;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SqlInjectionTest {
+
+    @Test
+    void testSqlInjection() throws SQLException {
+        Connection connection = ConnectionUtil.getDataSource().getConnection();
+        Statement statement = connection.createStatement();
+
+        String sql = """
+                SELECT * FROM cabang
+                                    
+                """;
+        ResultSet resultSet = statement.executeQuery(sql);
+
+        while (resultSet.next()) {
+
+            String KodeCabang = resultSet.getInt("kodeCabang");
+            String NamaCabang = resultSet.getString("NamaCabang");
+
+
+            System.out.println(
+                    String.join(",", KodeCabang, NamaCabang)
+            );
+        }
+
+
+        resultSet.close();
+        statement.close();
+        connection.close();
+    }
+}
