75cc99cbc8ed29c5f6df05dff73377b259c761f7
Rhendrik Rian <rhendrik20@gmail.com>
2024-01-09 22:03:53 +0700

    Menambahkan StatementTest.java

---
+++ src/test/java/belajar/maven/StatementTest.java
@@ -0,0 +1,40 @@
+package belajar.maven;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class StatementTest {
+
+    @Test
+    void testCreateStatement() throws SQLException {
+        Connection connection = ConnectionUtil.getDataSource().getConnection();
+        Statement statement = connection.createStatement();
+
+        statement.close();
+
+        connection.close();
+    }
+
+    @Test
+    void testExecuteUpdate() throws SQLException {
+        Connection connection = ConnectionUtil.getDataSource().getConnection();
+        Statement statement = connection.createStatement();
+
+        String sql = """
+                INSERT INTO Cabang (KodeCabang, NamaCabang) VALUES
+                (0203, 'Pancoran Barat'),
+                (0204, 'Kuningan Tengah'),
+                (0205, 'Pasar Minggu'),
+                (0206, 'Cilandak');
+                                    
+                """;
+        int update = statement.executeUpdate(sql);
+        System.out.println(update);
+
+        statement.close();
+        connection.close();
+    }
+}
